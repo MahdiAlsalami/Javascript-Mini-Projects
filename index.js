5519d82d1a02e6a7c65f6257f92d34f660f1e388
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-02-05 14:42:22 -0800

    Learnt console.log(); and document.getElementByID().textContext=;

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+
+//Learnt console.log(``); and document.getElementByID(``).textContext=``;
+
+console.log(`Hello, I am,`);
+console.log(`Mahdi Alsalami`)
+
+window.alert("BECAREFUL")
+
+
+document.getElementById(`myH1`).textContent="Hello";
+document.getElementById(`myP`).textContent=`I like pizza!`;
+
+*/
