8bf470b6540421da8769f2ed6842b180caa0ae91
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-02-05 15:16:22 -0800

    Created and understood variables

--- index.js
+++ index.js
@@ -11,4 +11,39 @@ window.alert("BECAREFUL")
 document.getElementById(`myH1`).textContent="Hello";
 document.getElementById(`myP`).textContent=`I like pizza!`;
 
+// learnt varibels: ex) let age = 19;
+//                      console.log(`You are ${age} years old!`)
+
+let nameFirst = "Mahdi Alsalami";
+let favFood = "Cookies";
+
+
+console.log(typeof nameFirst)
+console.log(`My favorite food is ${favFood}.`)
+
+*
+
+//Booleans
+
+let forSale = false
+console.log(`The bike is for sale!: ${forSale}`)
+
+let online = true
+console.log(`Are they online: ${online}`)
+
+
+
+let Fullname = "Mahdi Alsalami";
+let age = 19;
+let school = true
+
+document.getElementById(`p1`).textContent = `Your fullname is ${Fullname}`;
+document.getElementById(`p2`).textContent = `Your age is ${age}`;
+document.getElementById(`p3`).textContent = `You are in school:${school}`;
+
+
+//console.log(`Your name is ${Fullname}.`)
+//console.log(`Your age is ${age}.`)
+//console.log(`Im in school!: ${school}.`)
+
 */
