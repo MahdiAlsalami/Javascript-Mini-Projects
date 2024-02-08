dfaecaef1b90b7b4cd06cacaf8b5a903241dde5d
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-02-07 23:51:40 -0800

    Having trouble with css. Need to look over.

--- index.js
+++ index.js
@@ -91,3 +91,28 @@ console.log(students)
 
 */
 
+//Type Converisons:
+
+//let nameFull = window.prompt(`What is your name?:`)
+//let age = Number(window.prompt(`How old are you?:`))
+
+
+//age += 1
+//console.log(age, nameFull);
+
+//Constants:
+
+
+/*const PI = 3.14159;
+let radius;
+let circumference;
+
+document.getElementById(`mySubmit`).onclick = function(){
+  radius = document.getElementById(`myText`).value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById(`myH3`).textContent = circumference + ("cm")
+
+}
+*/
+
